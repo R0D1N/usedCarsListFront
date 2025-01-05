cb08c46d090c0b1a11e61c47f6e4a97252c5e11c
Kyrylo Rodin <k.rodin@joinposter.com>
2025-01-05 12:56:14 +0200

    using env data

--- src/api/carsApiConfig.js
+++ src/api/carsApiConfig.js
@@ -3,25 +3,25 @@ import { getData } from "./fetchData.js";
 const erpApiConfig = (baseURL) => [
   {
     name: "getCarById",
-    url: ({ id }) => `http://localhost:3000/api/v1/cars/${id}`,
+    url: ({ id }) => `${baseURL}/api/v1/cars/${id}`,
     method: getData,
     errorMessage: "Failed to get car",
   },
   {
     name: "getCarsList",
-    url: () => "http://localhost:3000/api/v1/cars",
+    url: () => `${baseURL}/api/v1/cars`,
     method: getData,
     errorMessage: "Failed to get cars list",
   },
   {
     name: "getBrandList",
-    url: () => "http://localhost:3000/api/v1/brandlist",
+    url: () => `${baseURL}/api/v1/brandlist`,
     method: getData,
     errorMessage: "Failed to get brands list",
   },
   {
     name: "getSeriesList",
-    url: ({ id }) => `http://localhost:3000/api/v1/serieslist/${id}`,
+    url: ({ id }) => `${baseURL}/api/v1/serieslist/${id}`,
     method: getData,
     errorMessage: "Failed to get series list",
   },
